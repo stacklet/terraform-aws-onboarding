6a5046ab156c28e373a32bb64f91fd519eefbe22
Alberto Donato <alberto@stacklet.io>
2025-04-07 18:58:58 +0200

    chore: add extra execution roles ARNs to output

--- access/outputs.tf
+++ access/outputs.tf
@@ -12,3 +12,8 @@ output "execution_role" {
   description = "ARN for the Stacklet execution role"
   value       = local.create_iam_resources ? aws_iam_role.execution[0].arn : null
 }
+
+output "execution_extra_roles" {
+  description = "ARNs for extra policy execution roles"
+  value       = length(aws_iam_role.execution_extra) > 0 ? [for r in aws_iam_role.execution_extra : r.arn] : null
+}
