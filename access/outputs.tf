2db20a1e0c4c986466363d65316978f8451ea19b
William Reade <william@stacklet.io>
2025-04-15 11:41:49 +0100

    fix: output descriptions

--- access/outputs.tf
+++ access/outputs.tf
@@ -1,19 +1,19 @@
 output "discover_role" {
-  description = "ARN for the Stacklet collector role"
+  description = "ARN for the resource-discovery role assumed by Stacklet AssetDB"
   value       = local.create_iam_resources ? aws_iam_role.discover[0].arn : null
 }
 
 output "forward_role" {
-  description = "ARN for the Stacklet forwarder role"
+  description = "ARN for the role used to forward cloud events to Stacklet"
   value       = local.create_forward_role ? aws_iam_role.forward[0].arn : null
 }
 
 output "execution_role" {
-  description = "ARN for the Stacklet execution role"
+  description = "ARN for the default policy-execution role assumed by Stacklet Execution"
   value       = local.create_iam_resources ? aws_iam_role.execution[0].arn : null
 }
 
 output "execution_extra_roles" {
-  description = "ARNs for extra policy execution roles"
+  description = "ARNs for extra policy-execution roles assumed by Stacklet Execution"
   value       = length(aws_iam_role.execution_extra) > 0 ? [for r in aws_iam_role.execution_extra : r.arn] : null
 }
